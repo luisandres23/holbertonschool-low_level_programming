33e4b6605ee1e7d6a9c8a096a7256b3619e1c9df
luisandres23 <luisandresorozco23@gmail.com>
2021-06-22 12:20:00 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,52 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - Printing combinations of two two-digit numbers.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j, k, l;
+int nb1, nb2;
 
-i = j = k = 48; l = 49;
-while  ((i < 58))
+for (nb1 = 0; nb1 <= 98; nb1++)
 {
-putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
-if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+for (nb2 = nb1 + 1; nb2 <= 99; nb2++)
 {
-putchar('\n'); i++;
-}
-else
-{
-putchar(44); putchar(32);
-if ((k == 57) && (l == 57))
-{
-if (j < 56)
-{
-l = ++j + 1; k = i;
-}
-else if (j == 56)
-{
-j++; k = i + 1; l = 48;
-}
-else if (j == 57)
-{
-j = 48; l = 49; k = ++i;
+putchar((nb1 / 10) + '0');
+putchar((nb1 % 10) + '0');
+putchar(' ');
+putchar((nb2 / 10) + '0');
+putchar((nb2 % 10) + '0');
+
+if (nb1 == 98 && nb2 == 99)
+break;
+
+putchar(',');
+putchar(' ');
 }
 }
-else if (l < 57)
-{
-l++;
-}
-else
-{
-l = 48; k++;
-}
-}
-}
+
+putchar('\n');
+
 return (0);
 }
